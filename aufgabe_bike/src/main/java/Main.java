07057bbf8135b8546a90ad84846a2769120a9c54
XxeLexX <105204254+XxeLexX@users.noreply.github.com>
2022-05-09 12:27:30 +0200

    Update Main.java

--- aufgabe_bike/src/main/java/Main.java
+++ aufgabe_bike/src/main/java/Main.java
@@ -10,8 +10,7 @@ public class Main {
         idCopy.run(readPath,writePath_0);
 
         // get ride_id with Flink
-        // test number of parallelism 1,2 and 3
-
+        // test number of parallelism 1 to 4
         IDCopy_Flink idCopy_flink = new IDCopy_Flink();
         for (int i = 1; i <= 4; i ++){
             String writePath_temp = "/Users/lxx/Desktop/Aufgaben/aufgabe_bike/src/main/resources/" + "para_" + i + "_id_withFlink.csv";
